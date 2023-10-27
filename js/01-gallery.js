09b13e31740eeb1b7b57d5e0ba16d0586b94e9ec
KaterynaRadchenko <82673991+Katia-Radchenko@users.noreply.github.com>
2023-10-27 19:11:56 +0300

    hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import {galleryItems} from './gallery-items.js';
+
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector('.gallery')
+list.insertAdjacentHTML('beforeend', createMarkup(galleryItems))
+list.addEventListener('click', onModal)
+
+function createMarkup(array) {
+    return array.map(({preview, original, description}) =>
+        ` <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('')
+
+}
+
+ function onModal(e) {
+     e.preventDefault()
+     if (e.target.nodeName !== "IMG") {
+         return;
+     }
+
+     const isItemImage = e.target.classList.contains('gallery__image');
+     if (!isItemImage){
+         return;
+     }
+
+     const currentImgUrl = e.target.dataset.source;
+
+     const instance = basicLightbox.create(
+         `<img src="${currentImgUrl}" width="800" height="600"/>`);
+     instance.show();
+
+ }
